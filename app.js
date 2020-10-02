ee0d41afa8bc1c6a92999801e81363a2a62a061b
Aichung <aichunghsueh@gmail.com>
2020-10-02 23:55:18 +0800

    modified index.html

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-(function(){
-
-  if(jQuery){
-    alert("success");
+var vm = new Vue({
+  el: '#app',
+  data: {
+      message: 'Hello'
+  },
+  computed: {
+      // a computed getter
+      reversedMessage: function () {
+      // `this` points to the vm instance
+      return this.message.split('').reverse().join('')
+      }
   }
-  
-}())
+})
